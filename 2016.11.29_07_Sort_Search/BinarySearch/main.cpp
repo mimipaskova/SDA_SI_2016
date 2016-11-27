6e5f31ab47bab23b967ed05cd43cb45d09423f4d
mimipaskova <maria.paskova@abv.bg>
2016-11-27 23:54:29 +0200

    Ad binary search which is bloken

---
+++ 2016.11.29_07_Sort_Search/BinarySearch/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int arr[100];
+
+int binarySearch(int x, int beginn, int endd) {
+    int centerEl = arr[(beginn + endd)/2];
+    if(beginn + 1 == endd) {
+        return -1;
+    }
+    if(x == centerEl) {
+        return (beginn + endd) /2;
+    }
+
+    if(x < centerEl) {
+        binarySearch(x, beginn, (beginn + endd) /2 + 1 );
+    } else {
+        binarySearch(x, (beginn + endd)/2 , endd);
+    }
+}
+
+int main()
+{
+    int size = 10;
+    for(int i = 0; i<10; i++) {
+        arr[i] = i+10;
+    }
+    cout<< binarySearch(10, 0, 10)<<endl;
+    cout<< binarySearch(14, 0, 10)<<endl;
+    cout<< binarySearch(15, 0, 10)<<endl;
+    cout<< binarySearch(20, 0, 10)<<endl;
+    cout << "Hello world!" << endl;
+    return 0;
+}
